309669b1385ce83bfb8a35a04cf2cbc4145cf92b
Ziyi Zhao <zz89@rice.edu>
2022-02-23 11:08:51 -0600

    Add group struct and function.

--- window-bit-count-apx/window-bit-count-apx.h
+++ window-bit-count-apx/window-bit-count-apx.h
@@ -15,6 +15,69 @@ uint64_t N_MERGES = 0; // keep track of how many bucket merges occur
 */
 
 typedef struct {
+    int idx;             // group index
+    int n_vallid;        // number of valid buckets 
+    int *buckets;           // circular array
+    int head, tail;     // index of the head and tail element
+    int k;              // algorithm parameter
+    int bucket_size;    // the size of each bucket
+} group_t;
+
+/* Init group instance. */
+group_t* creat_group(int idx, int k) {
+    group_t *ret = (group_t*) malloc(sizeof(group_t));
+    assert(ret != NULL);
+    ret->idx = idx;
+    ret->buckets = (int*) malloc((k + 1) * sizeof(int));
+    assert(ret->buckets != NULL);
+    ret->head = ret->tail = 0;
+    ret->k = k;
+    ret->bucket_size = pow(2, idx);
+    return ret;
+}
+
+void free_group(group_t* grp) {
+    free(grp->buckets);
+    free(grp);
+}
+
+/* add an element to group.
+ * @retval -1 no merge happens
+ * @retval timestamp if merge happens returns the timestamp of the emitted bucket
+ * @param grp pointer to group
+ * @param ts timestamp of input item
+ */
+int group_add(group_t* grp, int ts) {
+    int emit = -1;
+    // check if full --> need to merge
+    if (grp->n_vallid == (grp->k +1)) {
+        // merge: keep the latest timestamp
+        group_bpop(grp);
+        emit = group_bpop(grp);
+    }
+    group_bpush(grp, ts);
+    return emit;
+}
+
+static inline int group_bpop(group_t* grp) {
+    int ret = grp->buckets[grp->tail];
+    grp->tail++;
+    grp->tail %= (grp->k + 1);
+    grp->n_vallid--;
+    return ret;
+}
+
+static inline void group_bpush(group_t* grp, int ts) {
+    grp->buckets[grp->head] = ts;
+    grp->head++;
+    grp->head %= (grp->k + 1);
+    grp->n_vallid++;
+}
+
+inline int group_get_sum(group_t* grp) {
+    return grp->n_vallid * grp->bucket_size;
+}
+typedef struct {
     // TODO: Fill me.
 } StateApx;
 
